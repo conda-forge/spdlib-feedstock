12b8a4edac042a678c11c52ae34003d14269eb74
Sam Gillingham <gillingham.sam@gmail.com>
2019-03-25 15:43:10 +1000

    try new build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ `uname` == Darwin ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
-else
-    export LD_LIBRARY_PATH=$PREFIX/lib    
-fi
-
 cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D HDF5_INCLUDE_DIR=$PREFIX/include \
     -D HDF5_LIB_PATH=$PREFIX/lib \
