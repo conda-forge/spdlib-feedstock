3782f0ac58d6b9d0f0e5ad7e560862b9e3e9e356
Sam Gillingham <gillingham.sam@gmail.com>
2019-04-03 08:42:23 +1000

    don't build Python on Windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -29,38 +29,3 @@ if errorlevel 1 exit 1
 nmake install
 if errorlevel 1 exit 1
 
-:: now Python bindings
-:: Delete the static build of gdal so FindGDAL.cmake picks up gdal_i.lib
-del %LIBRARY_LIB%\gdal.lib
-cd python
-cmake -G "NMake Makefiles" -D CMAKE_INSTALL_PREFIX=%STDLIB_DIR% ^
-    -D CMAKE_BUILD_TYPE=Release ^
-    -D HDF5_INCLUDE_DIR=%LIBRARY_INC% ^
-    -D HDF5_LIB_PATH=%LIBRARY_LIB% ^
-    -D SPDLIB_IO_INCLUDE_DIR=%LIBRARY_INC% ^
-    -D SPDLIB_IO_LIB_PATH=%LIBRARY_LIB% ^
-    -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    .
-if errorlevel 1 exit 1
-
-nmake
-if errorlevel 1 exit 1
-
-nmake install
-if errorlevel 1 exit 1
-
-:: now the 'ng' python bindings
-cd ..\ngpython
-%PYTHON% setup.py build --gdalinclude=%LIBRARY_INC% ^
-    --boostinclude=%LIBRARY_INC% ^
-    --gslinclude=%LIBRARY_INC% ^
-    --cgalinclude=%LIBRARY_INC% ^
-    --lasinclude=%LIBRARY_INC% ^
-    --hdf5include=%LIBRARY_INC% ^
-    --gdallib=%LIBRARY_LIB%
-if errorlevel 1 exit 1
-
-%PYTHON% setup.py install
-if errorlevel 1 exit 1
-
-cd ..
